696c17ab08737b0a5d9e4e7251070e2506313707
peter <fr33wayz@gmail.com>
2015-10-23 15:00:02 +0100

    change it to index.html

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('browserify', function () {
 
 
 gulp.task('copy', function () {
-    gulp.src('src/nounOnline.html').pipe(gulp.dest('dist'))
+    gulp.src('src/index.html').pipe(gulp.dest('dist'))
 });
 
 
